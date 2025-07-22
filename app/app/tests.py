fdecf48cf03fc280dcf965b332722231f7323cd2
haddad207 <haddad0207@gmail.com>
2025-07-22 14:19:28 +0300

    added subtraction test

---
+++ app/app/tests.py
@@ -0,0 +1,34 @@
+"""
+Samle tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+
+    def test_add_numbers(self):
+        """Test adding two numbers together."""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_add_numbers_negative(self):
+        """Test adding two negative numbers together."""
+        res = calc.add(-5, -6)
+
+        self.assertEqual(res, -11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting two numbers."""
+        res = calc.subtract(10, 5)
+
+        self.assertEqual(res, 5)
+
+    def test_subtract_negative_numbers(self):
+        """Test subtracting two numbers."""
+        res = calc.subtract(-10, -5)
+
+        self.assertEqual(res, -5)
